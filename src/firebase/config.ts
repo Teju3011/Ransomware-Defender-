b48b8f46a631ca778523abc0ef39d4e8fda587a5
Teju3011 <tej301105@gmail.com>
2026-03-31 12:06:14 +0530

    Add files via upload

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-7075617176-73e72",
+  "appId": "1:671017917408:web:0bded13a2aa05d0241a985",
+  "apiKey": "AIzaSyDpN77vIVEhh1eErD0Ew9u660WMmEhVjqw",
+  "authDomain": "studio-7075617176-73e72.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "671017917408"
+};
